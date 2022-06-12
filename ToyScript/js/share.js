9850de935c8d8fd26ebe466ee2eabc02b6cee2a2
moko0428 <ljun925@naver.com>
2022-06-13 02:05:11 +0900

    공유하기 미구현

--- ToyScript/js/share.js
+++ ToyScript/js/share.js
@@ -1,34 +1,32 @@
-   function kakaoShare(){
-    Kakao.Link.sendDefault({
-        objectType: 'feed',
-        content: {
-        title: '오늘의 디저트',
-        description: '아메리카노, 빵, 케익',
-        imageUrl:
-            'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-        link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            androidExecutionParams: 'test',
-        },
-        },
-        social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
-        },
-        buttons: [
-        {
-            title: '웹으로 이동',
+const url = 'https://mokolovetype.netlify.app';
+    function setShare(){
+        var resultImg = document.querySelector('#resultImg');
+        var resultAlt = resultImg.firstElementChild.alt;
+        const shareTitle = '십이간지 연애유형 결과';
+        const shareDes = infoList[resultAlt].name;
+        const shareImage = url + 'img/image-' + resultAlt + '.png';
+        const shareURL = url + 'page/result-' + resultAlt + '.html';
+      
+        Kakao.Link.sendDefault({
+          objectType: 'feed',
+          content: {
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
             link: {
-            mobileWebUrl: 'https://developers.kakao.com',
+              mobileWebUrl: shareURL,
+              webUrl: shareURL
             },
-        },
-        {
-            title: '앱으로 이동',
-            link: {
-            mobileWebUrl: 'https://developers.kakao.com',
+          },
+      
+          buttons: [
+            {
+              title: '결과확인하기',
+              link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
+              },
             },
-        },
-        ]
-    });
-}
+          ]
+        });
+      }
