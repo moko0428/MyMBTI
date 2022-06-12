5dd99274784a440fd9a1d044c8b91abeb35b005d
moko0428 <ljun925@naver.com>
2022-06-12 23:40:30 +0900

    QnA 페이지 구현

--- ToyScript/js/start.js
+++ ToyScript/js/start.js
@@ -1,5 +1,50 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const endPoint = 12;
+
+function goResult(){
+  
+}
+
+function addAnswer(answerText, qIdx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+  answer.addEventListener("click",function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
+    }
+    setTimeout(() => {
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    }, 450)
+  }, false);
+}
+
+function goNext(qIdx){
+  if(++qIdx === endPoint){
+    goResult();
+  }
+  var q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+  var status = document.querySelector('.statusBar');
+  status.style.width = (100/endPoint) * (qIdx+1) + '%';
+
+}
 
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
@@ -11,8 +56,10 @@ function begin(){
       main.style.display = "none";
       qna.style.display = "block";
     }, 450)
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
-
+  
   //
   //
 }
