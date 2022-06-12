0175e143c0fa505ca6dcc5b5c4b1e993f55c0901
moko0428 <ljun925@naver.com>
2022-06-13 01:54:01 +0900

    공유하기 구현

---
+++ ToyScript/js/share.js
@@ -0,0 +1,34 @@
+   function kakaoShare(){
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+        title: '오늘의 디저트',
+        description: '아메리카노, 빵, 케익',
+        imageUrl:
+            'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            androidExecutionParams: 'test',
+        },
+        },
+        social: {
+        likeCount: 10,
+        commentCount: 20,
+        sharedCount: 30,
+        },
+        buttons: [
+        {
+            title: '웹으로 이동',
+            link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            },
+        },
+        {
+            title: '앱으로 이동',
+            link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            },
+        },
+        ]
+    });
+}
